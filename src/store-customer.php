4b4cea9c71a3e7dfa586f01909cb932f1b9f75bf
Rob Soltani - Sohrab-Dell <rob@soltani.io>
2022-08-11 06:50:58 -0400

    Added AddRole Function to StoreCustomer
    The StoreCustomer class now has a AddRole function that adds the "Store Customer" role.

--- src/store-customer.php
+++ src/store-customer.php
@@ -14,8 +14,40 @@ class StoreCustomer
     public static function Initiate()
     {
 
-
+        // Register the AddRole function with WordPress's initialization hook
+        add_action( 'init', 'MHS_INTERVIEW_TASK_2\StoreCustomer::AddRole', 10 );
 
     }
+
+
+    public static function AddRole()
+    {
+
+        // Check to see whether the role already exists
+        $StoreCustomerExistingRole = get_role( 'mhs_interview_task_2_store_customer' );
+        
+        if( is_null($StoreCustomerExistingRole) )
+        {
+            // Add the "Store Customer" role if it is not defined
+            add_role( 'mhs_interview_task_2_store_customer', 'Store Customer' );
+        }
+        else
+        {
+            
+            // Get current capabilities of the  role
+            $StoreCustomerExistingCapabilities = $StoreCustomerExistingRole->capabilities;
+            
+            // Iterate through the existing capabilities and remove them
+            foreach ($StoreCustomerExistingCapabilities as $StoreCustomerExistingCapabilityName => $StoreCustomerExistingCapabilityValue)
+            {
+
+                $StoreCustomerExistingRole->remove_cap( $StoreCustomerExistingCapabilityName );
+
+            }
+
+        }
+
+    }
+
 
 }
