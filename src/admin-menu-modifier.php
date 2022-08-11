21d4ff14d3bfaac5600352690d9aa178913b0dfd
Rob Soltani - Sohrab-Dell <rob@soltani.io>
2022-08-11 10:03:36 -0400

    AdminMenuModifier Class Added
    Created the AdminMenuModifier class with three funcitons. Initialize: adds the action to the admin_menu hook to execute the second function (RemoveUnwantedItemsFromAdminMenu). RemoveUnwantedItemsFromAdminMenu: removes the links to the Plugins, Appearance, Users, and Tools pages from the admin menu. IsUserAdministrator: implements the logic to detect whether user is administrator.

--- src/admin-menu-modifier.php
+++ src/admin-menu-modifier.php
@@ -13,8 +13,55 @@ class AdminMenuModifier
     public static function Initiate()
     {
 
-
-
+        // Register the RemoveUnwantedItemsFromAdminMenu function with WordPress's admin_menu hook
+        add_action('admin_menu', 'MHS_INTERVIEW_TASK_2\AdminMenuModifier::RemoveUnwantedItemsFromAdminMenu');
     }
 
+    private static function IsUserAdministrator()
+    {
+
+        // Get current logged in user
+        $CurrentUser = wp_get_current_user();
+
+        // Check if the current user if not null
+        if (!is_null($CurrentUser)) {
+
+            // Get current user roles
+            $CurrentUser_Roles = (array) $CurrentUser->roles;
+
+            // Check whether current user is a administrator
+            $CurrentUser_IsAdministrator = in_array('administrator', $CurrentUser_Roles, true);
+
+            if ($CurrentUser_IsAdministrator) {
+
+                return true;
+
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
+    }
+
+    public static function RemoveUnwantedItemsFromAdminMenu()
+    {
+
+        // Remove items from menu only if the user is an administrator
+        if (AdminMenuModifier::IsUserAdministrator()) {
+
+            // Remove the Plugins link from the admin menu
+            remove_menu_page('plugins.php');
+
+            // Remove the Appearance link from the admin menu
+            remove_menu_page('themes.php');
+            
+            // Remove the Users link from the admin menu
+            remove_menu_page('users.php');
+
+            // Remove the Tools link from the admin menu
+            remove_menu_page( 'tools.php' );
+
+        }
+    }
 }
